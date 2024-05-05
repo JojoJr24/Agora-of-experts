bf3b10e9510edbd2c950d7293c3cfe50a9e18c1d
Guillermo <guillermo.j.montenegro@gmail.com>
2024-05-05 00:23:22 -0300

    Cambio en el layout

--- ControllerSettings/settings_ui.py
+++ ControllerSettings/settings_ui.py
@@ -6,13 +6,13 @@ from ControllerSettings.settings_logic import getEmbeddingModel, getVisionModel,
 def settings_tab():
     with gr.Blocks() as tab:
         with gr.Row():
-            reload_button = gr.Button("Restart AoE", min_width=1, variant="primary" ) 
-            close_button = gr.Button("Close AoE", min_width=1, variant="secondary" ) 
-
-        with gr.Row():
             vision_model = gr.Dropdown(label="Vision Model", choices=get_model_list() , value=getVisionModel())  # Add your options here
             embedding_model = gr.Dropdown(label="Embedding Model **Note:** Changing the Embedding Model will break existing Collections.", choices=get_model_list(), value=getEmbeddingModel())  # Add your options here
 
+        with gr.Row():
+            reload_button = gr.Button("Restart AoE", min_width=1, variant="primary" ) 
+            close_button = gr.Button("Close AoE", min_width=1, variant="secondary" ) 
+      
         reload_button.click(None, js="location.reload()")
         close_button.click(fn=quit)
         vision_model.select(setVisionModel,[ vision_model])
